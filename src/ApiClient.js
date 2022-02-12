d6b52ccc69b5716d622f80b663c734b06254c8de
sherbann <93603838+sherbann@users.noreply.github.com>
2022-02-12 02:29:56 +0000

    prep for heroku

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -11,16 +11,17 @@ export class ApiClient {
     }
   }
   // getting data from API
-  getWeather(lat = 53.382969, lon = -1.4659) {
-    return this.getRequest(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=06af2c84a95e6a736fd7bab4b3be279d`)
-    
+  getWeather(lat = 53.383331, lon = -1.466667) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=06af2c84a95e6a736fd7bab4b3be279d`
+    return this.getRequest(apiUrl)
+
   }
 
   // in case of error is showing an alert message with error
   getRequest(url) {
     // create a promise for the axios request
     return axios.get(url)
-    // using .then, create a new promise which extracts the data
+      // using .then, create a new promise which extracts the data
       .then(this.status)
       .catch(function (error) {
         console.error(error);
