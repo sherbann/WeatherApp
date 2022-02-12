f06448c7a15bc6c3d50afaa5528700aba58ba05e
sherbann <93603838+sherbann@users.noreply.github.com>
2022-02-12 00:54:54 +0000

    minor change

--- src/App.js
+++ src/App.js
@@ -28,33 +28,33 @@ class App extends React.Component {
 
 
   getSheffield() {
-    this.fetchWeatherData(53.382969, -1.4659,"Sheffield")
+    this.fetchWeatherData(53.382969, -1.4659, "Sheffield")
     //window.location.reload();
 
   }
-    //*retrieving <Card> form from Scard.js with values from API
+  //*retrieving <Card> form from Scard.js with values from API
   getHyderabad() {
-    this.fetchWeatherData(17.38405, 78.45636,"Hyderabad")
+    this.fetchWeatherData(17.38405, 78.45636, "Hyderabad")
     //window.location.reload();
 
   }
   getParis() {
-    this.fetchWeatherData(48.864716, 2.349014,"Paris")
+    this.fetchWeatherData(48.864716, 2.349014, "Paris")
     //window.location.reload();
 
   }
   getBarcelona() {
-    this.fetchWeatherData(41.390205, 2.154007,"Barcelona")
+    this.fetchWeatherData(41.390205, 2.154007, "Barcelona")
     //window.location.reload();
 
   }
   getTokyo() {
-    this.fetchWeatherData(35.6895, 139.69171,"Tokyo")
+    this.fetchWeatherData(35.6895, 139.69171, "Tokyo")
     //window.location.reload();
 
   }
 
-  fetchWeatherData(lat, lon ,location) {
+  fetchWeatherData(lat, lon, location) {
     this.setState({
       loading: "....loading",
       fetching: true,
@@ -62,7 +62,7 @@ class App extends React.Component {
     })
 
     this.apiClient.getWeather(lat, lon)
-      .then((response) => { this.updateWeather(response.data.daily,response.data.timezone_offset,location) })
+      .then((response) => { this.updateWeather(response.data.daily, response.data.timezone_offset, location) })
       .finally(() => {
         this.setState({
           loading: ""
@@ -70,7 +70,7 @@ class App extends React.Component {
       })
   }
 
-  updateWeather(response,timezone_offset,location) {
+  updateWeather(response, timezone_offset, location) {
     this.setState({
       weather: response,
       timezone_offset: timezone_offset,
@@ -102,28 +102,28 @@ class App extends React.Component {
   render() {
     return (
       <>
-      
-        <Container className="flex-container" id="container" style={{ backgroundImage: `url(${rainBackgroundAnimation})`}} >
+
+        <Container className="flex-container" id="container" style={{ backgroundImage: `url(${rainBackgroundAnimation})` }} >
           <Navbar collapseOnSelect="true" class="fluid" variant="dark" id="Navbar" expand="md" type="toggleButton">
             <Navbar.Brand className="font-weight-bold" id="NavBrand">Weather Forecast {this.state.loading}</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" aria-expanded="false" className="navbar-toggler" id="navbar-toggler"/>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" aria-expanded="false" className="navbar-toggler" id="navbar-toggler" />
             <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav" type="button" data-toggle="collapse">
-              <Nav  className="me-auto flex-container fit-content" id="cities">
-                <Nav.Link className="main-nav" style={this.state.location=="Sheffield"?{color: "yellow"}:{}} onClick={() => this.getSheffield()}>Sheffield</Nav.Link>
-                <Nav.Link className="main-nav" style={this.state.location == "Hyderabad" ? { color: "yellow" } : {}} onClick={() => this.getHyderabad()} >Hyderabad</Nav.Link>
-                <Nav.Link className="main-nav" style={this.state.location == "Paris" ? { color: "yellow"}:{}} onClick={() => this.getParis()}>Paris</Nav.Link>
-                <Nav.Link className="main-nav" style={this.state.location == "Barcelona" ? { color: "yellow" } : {}} onClick={() => this.getBarcelona()}>Barcelona</Nav.Link>
-                <Nav.Link className="main-nav" style={this.state.location == "Tokyo" ? { color: "yellow" } : {}} onClick={() => this.getTokyo()}>Tokyo</Nav.Link>
+              <Nav className="me-auto flex-container fit-content" id="cities">
+                <Nav.Link className="main-nav" style={this.state.location === "Sheffield" ? { color: "yellow" } : {}} onClick={() => this.getSheffield()}>Sheffield</Nav.Link>
+                <Nav.Link className="main-nav" style={this.state.location === "Hyderabad" ? { color: "yellow" } : {}} onClick={() => this.getHyderabad()} >Hyderabad</Nav.Link>
+                <Nav.Link className="main-nav" style={this.state.location === "Paris" ? { color: "yellow" } : {}} onClick={() => this.getParis()}>Paris</Nav.Link>
+                <Nav.Link className="main-nav" style={this.state.location === "Barcelona" ? { color: "yellow" } : {}} onClick={() => this.getBarcelona()}>Barcelona</Nav.Link>
+                <Nav.Link className="main-nav" style={this.state.location === "Tokyo" ? { color: "yellow" } : {}} onClick={() => this.getTokyo()}>Tokyo</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
-          <Today  state={this.state.curDT}/>
-         
+          <Today state={this.state.curDT} />
+
           <Row className="row-gap row-cols-xs-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
             {this.buildCards()}
           </Row>
         </Container>
-       
+
       </>
     );
   }
